d5341b4442959b0d3e13cd7cfd89d2e95e96a963
Firefoxmoo89 <firefoxmoo@gmail.com>
2024-10-18 22:55:09 -0600

    Added popup for item listing control, changed styling, changed population to come from object data

---
+++ script/data.js
@@ -0,0 +1,18 @@
+var itemData = {
+	1212: { 
+		"name": "Banana",
+		"category": "food",
+		"price": "5",
+		"shortDescription": "totes goodo",
+		"description": "Bananas are the best to eat at weddings because they give potassium, which is also incredible flammable so that's awesome. Enjoy!",
+		"imgCount": 1
+	},
+	9245: {
+		"name": "Cookies",
+		"category": "food",
+		"price": "12",
+		"shortDescription": "totes goodo",
+		"description": "These are dangerously good, so don't let kiddos eat them, or you may not live to see the light of day.",
+		"imgCount": 1
+	}
+}
