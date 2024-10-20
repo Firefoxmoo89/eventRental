e55228ce87fb316c24267b6d5e139c8bdbc306b0
Firefoxmoo89 <firefoxmoo@gmail.com>
2024-10-20 16:34:47 -0600

    Minor changes

--- script/data.js
+++ script/data.js
@@ -2,7 +2,7 @@ var itemData = {
 	1212: { 
 		"name": "Banana",
 		"category": "food",
-		"price": "5",
+		"price": 5,
 		"shortDescription": "totes goodo",
 		"description": "Bananas are the best to eat at weddings because they give potassium, which is also incredible flammable so that's awesome. Enjoy!",
 		"imgCount": 1
@@ -10,7 +10,7 @@ var itemData = {
 	9245: {
 		"name": "Cookies",
 		"category": "food",
-		"price": "12",
+		"price": 12,
 		"shortDescription": "totes goodo",
 		"description": "These are dangerously good, so don't let kiddos eat them, or you may not live to see the light of day.",
 		"imgCount": 1
